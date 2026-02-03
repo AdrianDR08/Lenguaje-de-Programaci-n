004de6984a19d4eef4c62bb475c294f42132c398
AdrianDR08 <jknxbz4xjt@privaterelay.appleid.com>
2026-02-03 14:51:53 -0400

    actualizar

---
+++ Radiobutton/Form1.cs
@@ -0,0 +1,31 @@
+namespace Radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                labelResultado.Text = "Seleccionaste: Opción 1";
+            }
+            else if (radioButton2.Checked)
+            {
+                labelResultado.Text = "Seleccionaste: Opción 2";
+            }
+            else if (radioButton3.Checked)
+            {
+                labelResultado.Text = "Seleccionaste: Opción 3";
+            }
+            else
+            {
+                labelResultado.Text = "No seleccionaste ninguna opción";
+            }
+        }
+
+    }
+}
